8af346d96f8db2b4bacb6fe12de69726c1f5a929
LanMas <ffoxic232@gmail.com>
2026-06-08 21:56:13 +0700

    sone chagnes for hearts UI

--- sub-latron/Scense/UI/hearts_bar_ui.gd
+++ sub-latron/Scense/UI/hearts_bar_ui.gd
@@ -16,7 +16,7 @@ var clear_len_rsgtr : int = len(hearts_regstr) - 1
 @export var count_hearts : int 
 
 func _ready() -> void:
-	for i in range(3):
+	for i in range(15):
 		update_hearts(OPERATION.ADD)
 	Eventbus.player_taked_damage.connect(_on_player_taked_damage)
 
@@ -43,6 +43,7 @@ func _on_player_taked_damage(amount : int):
 		if hearts_regstr[clear_len_rsgtr] == 0:
 			clear_len_rsgtr -= 1
 			print("Next heart regstr to: ", clear_len_rsgtr)
+	$Label.text = str(hearts_regstr)
 	
 	
 	print(hearts_regstr)
