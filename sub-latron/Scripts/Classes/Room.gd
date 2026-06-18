e76ec7518eca1f61009d8cce29dbcdeacb6a355f
LanMas <ffoxic232@gmail.com>
2026-06-18 15:20:55 +0700

    room_generator

--- sub-latron/Scripts/Classes/Room.gd
+++ sub-latron/Scripts/Classes/Room.gd
@@ -1,15 +1,18 @@
 extends TileMapLayer
 class_name Room
 
-var center : int
 var width : int
 var height : int
 var wall_coord : Vector2i
 var floor_coord : Vector2i
+var offect : Vector2i
 
-func generate(wall_coord : Vector2i, floor_coord : Vector2i, course_id : int , center : int , width : int , height : int):
-	_create_floor(floor_coord, course_id, width, height)
-	_create_walls(wall_coord, course_id, width, height)
+func generate(ptrwall_coord : Vector2i, ptrfloor_coord : Vector2i, course_id : int , global_positon_offect : Vector2i , ptrwidth : int , ptrheight : int):
+	offect = global_positon_offect
+	width = ptrwidth
+	height = ptrheight
+	wall_coord = ptrwall_coord
+	floor_coord = ptrfloor_coord
 
 func _create_floor(floor_coord: Vector2i, course_id : int , width : int, height: int):
 	for w in range(width):
@@ -18,6 +21,8 @@ func _create_floor(floor_coord: Vector2i, course_id : int , width : int, height:
 
 func _create_walls(wall_coord: Vector2i, course_id : int , width : int, height: int):
 	for w in range(width):
-		set_cell(Vector2i(w, ), course_id, wall_coord)
-		set_cell(Vector2i(w, height ), course_id, wall_coord)
-	for
+		set_cell(Vector2i(w + offect.x, 0 + offect.y), course_id, wall_coord)
+		set_cell(Vector2i(w + offect.x, height +  + offect.y), course_id, wall_coord)
+	for h in range(height):
+		set_cell(Vector2i(0 + offect.x, h + offect.y), course_id, wall_coord)
+		set_cell(Vector2i(width + offect.x, h + offect.y), course_id, wall_coord)
